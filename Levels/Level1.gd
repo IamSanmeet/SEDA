a54a273356d7819995105c1361cf634bc0103c54
JackNewt <122556891+JackNewt@users.noreply.github.com>
2023-04-20 08:46:25 -0400

    Made movable character

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
 
-func _physics_process(delta):
-	$Camera2D.postition = $Player.position
+func _physics_process(_delta):
+	var c = get_node_or_null("Player")
+	if c != null:
+		$Camera2D.position.y = c.position.y
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,24 +1,39 @@
 extends KinematicBody2D
 
-export var velocity = Vector2.ZERO
-export var gravity = 1
-export var speed = 5
-export var dv= Vector2.ZERO 
+var velocity = Vector2.ZERO
+var gravity = 1
+var speed = 5
+var dv = Vector2.ZERO 
 
 
 func _physics_process(_delta):
-
 	position += velocity
-
-	velocity = move_and_slide(dv)
-	if is_on_wall() == false or is_on_floor() == false:
-		print("false")
+	dv = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		dv.x -= speed
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.set_animation("Run")
+	elif Input.is_action_pressed("right"):
+		dv.x += speed
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.set_animation("Run")
+	else:
+		$AnimatedSprite.set_animation("Idle")
+	if is_on_floor() == false:
 		dv.y += gravity
-	if is_on_wall() == true and dv.y != 0 or is_on_floor() == true and dv.y != 0:
-		dv.y -= 1
-		print("true")
-		
-	
-	if Input.is_action_just_pressed("jump"):
-		dv.y += 40
-		
+		$AnimatedSprite.set_animation("Jump")
+	elif is_on_floor() == true:
+		dv.y = 0
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		dv.y -= 35
+	velocity += move_and_slide(dv, Vector2.UP)
+	velocity.x = clamp(velocity.x, -18, 18)
+	if velocity.x > 0 and dv.x == 0:
+		velocity.x -= 2
+	if velocity.x < 0 and dv.x == 0:
+		velocity.x += 2
+
+
+func _on_Timer_timeout():
+	print(velocity)
+	print(is_on_floor())
