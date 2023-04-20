1448a9c3f601a1f9ca71f65f4c732400017b7e6a
JackNewt <122556891+JackNewt@users.noreply.github.com>
2023-04-20 09:25:48 -0400

    Player is Functional!

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,39 +1,38 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var gravity = 1
-var speed = 5
+var gravity = 50
+var speed = 50
 var dv = Vector2.ZERO 
 
 
 func _physics_process(_delta):
-	position += velocity
+
 	dv = Vector2.ZERO
 	if Input.is_action_pressed("left"):
 		dv.x -= speed
 		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.set_animation("Run")
 	elif Input.is_action_pressed("right"):
 		dv.x += speed
 		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.set_animation("Run")
-	else:
-		$AnimatedSprite.set_animation("Idle")
 	if is_on_floor() == false:
 		dv.y += gravity
-		$AnimatedSprite.set_animation("Jump")
+
 	elif is_on_floor() == true:
-		dv.y = 0
+		velocity.y = 0
+		velocity.x = lerp(velocity.x, 0, .1)
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		dv.y -= 35
-	velocity += move_and_slide(dv, Vector2.UP)
-	velocity.x = clamp(velocity.x, -18, 18)
-	if velocity.x > 0 and dv.x == 0:
-		velocity.x -= 2
-	if velocity.x < 0 and dv.x == 0:
-		velocity.x += 2
-
-
+		dv.y -= 2000
+	velocity += dv
+	velocity.x = clamp(velocity.x, -300, 300)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if abs(velocity.x) < 1 and $AnimatedSprite.animation != "Idle":
+		$AnimatedSprite.set_animation("Idle")
+	if abs(velocity.x) >= 1 and $AnimatedSprite.animation != "Run":
+		$AnimatedSprite.set_animation("Run")
+	if abs(velocity.y) != 0 and $AnimatedSprite.animation != "Jump":
+		$AnimatedSprite.set_animation("Jump")
+		
 func _on_Timer_timeout():
 	print(velocity)
 	print(is_on_floor())
